e5ed25a42ec118afce248b49a01d8e5fba6b3ef6
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-05-15 10:29:21 +0300

    7.1.1. Dart nullability: Mini-exercise 1

--- bin/mini_exercise.dart
+++ bin/mini_exercise.dart
@@ -1,3 +1,10 @@
 void main() {
-  print('Hello world!');
+  /// Dart Nullability
+  ///
+  /// Null overview: Mini-exercise 1
+  ///
+  /// Create a String? variable called profession, but don’t give it a value.
+  /// Then you’ll have profession null. :]
+  String? profession;
+  print(profession);
 }
